44b3a89685bfd684daefead0fc2aabd6c97423f1
ryanzhang5 <zyh_840819@163.com>
2018-11-16 00:54:55 +0800

    te

--- examui3/src/app/app.module.ts
+++ examui3/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExamComponent } from './exam/exam.component';
+import { HeroComponent } from './hero/hero.component';
+import { HeroesComponent } from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExamComponent,
+    HeroComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
